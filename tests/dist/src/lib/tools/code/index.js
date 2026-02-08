20ca3aa2656b63bf299e4c4ccd31472fab3ed17b
Stephen Zweibel <oneperstephen@gmail.com>
2026-02-08 17:49:13 -0500

    Ignore and untrack tests/dist artifacts

--- tests/dist/src/lib/tools/code/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createExecuteTool = void 0;
-var execute_1 = require("./execute");
-Object.defineProperty(exports, "createExecuteTool", { enumerable: true, get: function () { return execute_1.createExecuteTool; } });
--- tests/dist/src/lib/tools/transform/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sortTool = exports.pickTool = exports.filterTool = void 0;
-var filter_1 = require("./filter");
-Object.defineProperty(exports, "filterTool", { enumerable: true, get: function () { return filter_1.filterTool; } });
-var pick_1 = require("./pick");
-Object.defineProperty(exports, "pickTool", { enumerable: true, get: function () { return pick_1.pickTool; } });
-var sort_1 = require("./sort");
-Object.defineProperty(exports, "sortTool", { enumerable: true, get: function () { return sort_1.sortTool; } });
